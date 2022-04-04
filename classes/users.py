cf2a8c08bb8899b699e50ce691e5ddfd1ecb2abe
enjiruuuu <fightingdreamer99@gmail.com>
2022-04-04 22:08:50 +0800

    add method to generate uuid

--- classes/users.py
+++ classes/users.py
@@ -1,15 +1,16 @@
 import hashlib
+import uuid
 from flask import request
+from datetime import datetime
 from classes.dynamodb import DynamodbAPI
 from boto3.dynamodb.conditions import Key
 
 class UsersApi:
     def __init__(self) -> None:
-        self.url = "users"
         self.tableName = "users"
 
         dynamodbInstance = DynamodbAPI()
-        self.table = dynamodbInstance.dynamodb.Table('users')
+        self.table = dynamodbInstance.dynamodb.Table(self.tableName)
     
     def __get(self, email):
         response = self.table.query(
@@ -20,6 +21,13 @@ class UsersApi:
     def __checkPassword(self, queryPassword, dbPassword) -> bool:
         hashed = hashlib.sha256(str(queryPassword).encode()).hexdigest()
         return bool(hashed == dbPassword)
+
+    def __generateUuid(self) -> str: 
+        now = datetime.now()
+        now = now.strftime("%m/%d/%Y%H:%M:%S")
+        generatedUuid = uuid.uuid5(uuid.NAMESPACE_DNS, now)
+        print(generatedUuid)
+        return generatedUuid
 
     def login(self) -> bool:
         input_json = request.get_json()
